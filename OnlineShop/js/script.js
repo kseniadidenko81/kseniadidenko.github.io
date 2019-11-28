6b54d23276676bff6a4ce4d1348506f0c8200e4d
kseniadidenko81 <57663840+kseniadidenko81@users.noreply.github.com>
2019-11-28 03:52:57 +0200

    change

--- OnlineShop/js/script.js
+++ OnlineShop/js/script.js
@@ -108,15 +108,53 @@ var slider3 = multiItemSlider('.slider3');
 //End Slider
 
 //----------------------------SLIDESHOW--------------------------
-var slides = document.querySelectorAll('#container-slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,3000);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function nextSlide() {
-	slides[currentSlide].className = 'slide';
-	currentSlide = (currentSlide+1)%slides.length;
-	slides[currentSlide].className = 'slide showing';
+function plusSlides(n) {
+	showSlides(slideIndex += n);
 }
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("slide");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" showing", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " showing";
+}
+
+function showSlides() {
+	var i;
+	var slides = document.getElementsByClassName("slide");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {slideIndex = 1}
+	slides[slideIndex-1].style.display = "block";
+	setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+// var slides = document.querySelectorAll('#container-slides .slide');
+// var currentSlide = 0;
+// var slideInterval = setInterval(nextSlide,3000);
+//
+// function nextSlide() {
+// 	slides[currentSlide].className = 'slide';
+// 	currentSlide = (currentSlide+1)%slides.length;
+// 	slides[currentSlide].className = 'slide showing';
+// }
 
 //----------------------------jQuery Scripts--------------------------
 (function ($) {
